8b4bd195b11cbaf0fd6e99217db3863b82954174
Hussain Nagaria <hussainbhaitech@gmail.com>
2025-10-28 16:51:52 +0530

    fix: linter

--- buzz/patches/rename_doctypes_for_buzz.py
+++ buzz/patches/rename_doctypes_for_buzz.py
@@ -1,7 +1,7 @@
 import frappe
 
 doctypes = {
-    "Event Management Settings": "Buzz Settings",
+	"Event Management Settings": "Buzz Settings",
 	"Buzz Event": "Buzz Event",
 }
 
@@ -10,12 +10,14 @@ def execute():
 	rename_doctypes()
 	create_sequences()
 
+
 def rename_doctypes():
 	for old in doctypes:
 		new = doctypes[old]
 		if not frappe.db.exists("DocType", new):
 			print(f"Renaming {old} to {new}")
 			frappe.rename_doc("DocType", old, new, force=True, ignore_if_exists=True)
+
 
 def create_sequences():
 	doctype = "Buzz Event"
@@ -24,6 +26,4 @@ def create_sequences():
 	last_name = frappe.db.get_all(doctype, fields=["max(name) as last"])[0].last
 	start_value = (last_name or 0) + 1
 	print(f"Creating sequence for {doctype} starting at {start_value}")
-	frappe.db.create_sequence(
-		doctype, start_value=start_value, check_not_exists=True
-	)
+	frappe.db.create_sequence(doctype, start_value=start_value, check_not_exists=True)
